91b6949b69130d944417fd21e8b8c8d37170af62
Harish Kadamudi <harishyouk@gmail.com>
2018-11-07 10:03:02 +0530

    correcting app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from security import authenticate, identity
+from code.security import authenticate, identity
 from resources.item import Item, ItemList
 from resources.user import UserRegistration
 from resources.store import Store,StoreList
